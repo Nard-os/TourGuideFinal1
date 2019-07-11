27f33e2a4d39deda925571b824526c9f47c29ae5
Nardos <nardosmelkamu21@gmail.com>
2019-07-11 04:17:24 -0700

    Fourth Commit

--- app/src/main/java/com/example/tourguidefinal/Dao/ProfileDao.kt
+++ app/src/main/java/com/example/tourguidefinal/Dao/ProfileDao.kt
@@ -1,16 +1,22 @@
 package com.example.tourguidefinal.Dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.tourguidefinal.Data.Profile
 
 @Dao
 interface ProfileDao {
     @Query("SELECT * FROM profiles WHERE user_name=:user_name")
-    fun getProfileByuser_name(user_name:Long): Profile
+    fun getProfilesByuser_name(user_name:Long): Profile
+    fun getAllProfiles(username: Long.Companion):LiveData<Profile>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
         fun insertusername(profile: Profile):Long
 
     @Update
     fun updateProfile(profile: Profile): Long
+
+    abstract fun deleteProfile(profile: Profile)
+    fun insertProfiles(profile: Profile)
+    fun getProfile(username: Long): Any
 }
