6932439df139e20cdae39e238fd65703f46fc385
Nardos <nardosmelkamu21@gmail.com>
2019-07-11 02:56:09 -0700

    Third Commit

--- app/src/main/java/com/example/tourguidefinal/Data/Profile.kt
+++ app/src/main/java/com/example/tourguidefinal/Data/Profile.kt
@@ -3,10 +3,13 @@ package com.example.tourguidefinal.Data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "profiles")
 data class Profile (
     @PrimaryKey @ColumnInfo(name = "user_name") val user_name:String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "email") val email:Long,
+    @ColumnInfo(name="country")val country: String,
     @ColumnInfo(name = "password") val password:Long)
+    :Serializable
