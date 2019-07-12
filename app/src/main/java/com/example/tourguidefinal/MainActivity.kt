82c8f894b7c750d85749080adfe52665ddeb5a25
Nardos <nardosmelkamu21@gmail.com>
2019-07-11 21:55:41 -0700

    REST API Commit

--- app/src/main/java/com/example/tourguidefinal/MainActivity.kt
+++ app/src/main/java/com/example/tourguidefinal/MainActivity.kt
@@ -14,53 +14,28 @@ import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var myDatabase: MyDatabase
+    private lateinit var myDatabase: MyDatabase
     private lateinit var profileDao: ProfileDao
     private lateinit var countryDao: CountryDao
     private lateinit var reviewDao: ReviewDao
 
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     fun onCreate(savedInstanceState: Bundle?, recyclerView: Any) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        AsyncTask.execute{
-        myDatabase= MyDatabase.getDatabase(this)
-        profileDao = myDatabase.profileDao()
-        countryDao = myDatabase.countryDao()
-        reviewDao = myDatabase.reviewDao()}
+        AsyncTask.execute {
+            myDatabase = MyDatabase.getDatabase(this)
+            profileDao = myDatabase.profileDao()
+            countryDao = myDatabase.countryDao()
+            reviewDao = myDatabase.reviewDao()
+        }
 
 
-        recyclerView_main.layoutManager = ConstraintLayout(this)
-        recyclerView.adapter = MainAdapter()
-
-        fetchJson()
-    }
-    fun fetchJson(){
-        println()
-        val url = "http://www.holiday-to-ethiopia.com/"
-        val request = Request.Builder().url(url).build()
-         val client = OkHttpClient()
-        client.newCall(request).enqueue(object: Callback{
-
-            override fun onResponse(call: Call, response: Response) {
-                val body = response?.body()?.string()
-                println(body)
-
-                val gson = GsonBuilder().create()
-               val tours = gson.fromJson(body, Tours::class.java)
-
-                runOnUiThread {
-                    val recyclerView_main
-                    recyclerView_main.adapter = MainAdapter(tours)
-                }}
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute")
-            }
-        })
     }
 }
-class Tours(val images: List<Images>)
-class Images(val id:Long, val name: String)
+
+
+
 
 
