883154d5c1e32ece5e95a572fed9f6dfbecbf12a
Nardos <nardosmelkamu21@gmail.com>
2019-07-11 20:48:17 -0700

    Commit

--- app/src/main/java/com/example/tourguidefinal/MainActivity.kt
+++ app/src/main/java/com/example/tourguidefinal/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.tourguidefinal
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import com.example.tourguidefinal.Dao.CountryDao
 import com.example.tourguidefinal.Dao.ProfileDao
 import com.example.tourguidefinal.Dao.ReviewDao
 import com.example.tourguidefinal.Database.MyDatabase
 import com.example.tourguidefinal.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
   private lateinit var myDatabase: MyDatabase
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var reviewDao: ReviewDao
 
 private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    fun onCreate(savedInstanceState: Bundle?, recyclerView: Any) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
@@ -26,5 +29,38 @@ private lateinit var binding: ActivityMainBinding
         profileDao = myDatabase.profileDao()
         countryDao = myDatabase.countryDao()
         reviewDao = myDatabase.reviewDao()}
+
+
+        recyclerView_main.layoutManager = ConstraintLayout(this)
+        recyclerView.adapter = MainAdapter()
+
+        fetchJson()
+    }
+    fun fetchJson(){
+        println()
+        val url = "http://www.holiday-to-ethiopia.com/"
+        val request = Request.Builder().url(url).build()
+         val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback{
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+               val tours = gson.fromJson(body, Tours::class.java)
+
+                runOnUiThread {
+                    val recyclerView_main
+                    recyclerView_main.adapter = MainAdapter(tours)
+                }}
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute")
+            }
+        })
     }
 }
+class Tours(val images: List<Images>)
+class Images(val id:Long, val name: String)
+
+
